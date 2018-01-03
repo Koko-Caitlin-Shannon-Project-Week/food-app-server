4a7930f7645faefbce5cd8388166e925e986be54
Koko Kassa <kowserk7@gmail.com>
2018-01-03 15:09:49 -0800

    fixing the API

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.get('/api/v1/recipes/search', (req, res) =>{
   superagent.get(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
   .then (result => res.send(result))
 });
+app.get('/api/v1/recipes/search/:ing', (req, res) =>{
+  superagent.get(`https://api.edamam.com/search?q=${req.params.ing}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  .then (result => res.send(result))
+});
 
 app.get('/api/v1/recipes/:id', (req,res) => {
   client.query(`SELECT * FROM recipes WHERE user_id = ${req.params.id};`)
@@ -43,21 +47,5 @@ app.get('/api/v1/recipes/:id', (req,res) => {
 });
 
 
-// app.get('/api/v1/recipes/find/:day_id', (req,res) =>{
-//   superagent.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/228234/analyzedInstructions?stepBreakdown=true`)
-//   .set('X-Mashape-Key', 'API_KEY')
-//   .set('Accept', 'application/json')
-//   .then(res => send(res))
-//   .catch(res => console.error(res));
-
-// })
-
 //app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/324694/analyzedInstructions?stepBreakdown=true")
-// .header("X-Mashape-Key", "")
-// .header("Accept", "application/json")
-// .end(function (result) {
-//   console.log(result.status, result.headers, result.body);
-// });
