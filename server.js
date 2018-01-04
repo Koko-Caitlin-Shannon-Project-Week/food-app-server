5457d32257405a2dc3de17fb9b2a7129028749e3
Shannon Tully <scatman.st@gmail.com>
2018-01-04 09:47:52 -0800

    lil fix

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/api/v1/recipes/:id', (req,res) => {
 
 app.put('/api/v1/recipes/:id/:day', (req, res) => {
   console.log('hit here')
-  console.log(req)
+  //console.log(req)
   let {json} = req.body;
   client.query(`UPDATE recipes SET ${req.params.day} = ($1) WHERE user_id = ${req.params.id};`,
 [json])
