0f3c03dedeffe3ea69bb32bdb405c3e36a4b728a
Koko Kassa <kowserk7@gmail.com>
2018-01-03 17:40:48 -0800

    working on our endpoint

--- server.js
+++ server.js
@@ -46,11 +46,10 @@ app.get('/api/v1/recipes/:id', (req,res) => {
   .catch (console.error);
 });
 
-app.put('/api/v1/recipes/:id', (req, res) => {
-  console.log('hit here');
-  console.log(req.headers.query);
+app.put('/api/v1/recipes/:id/:day', (req, res) => {
+  console.log('hit here')
   let {json} = req.body;
-  client.query(`UPDATE recipes SET ${req.query.day} = ($1) WHERE user_id = ${req.query.id};`,
+  client.query(`UPDATE recipes SET ${req.params.day} = ($1) WHERE user_id = ${req.params.id};`,
 [json])
 });
 
